825bd8b08bb6ca9b84dba6bcae277d91c348bcb7
Irina <sabaliauskieneirina@gmail.com>
2022-04-14 23:52:20 +0200

    styles header

--- src/Components/CardsList/CardsList.jsx
+++ src/Components/CardsList/CardsList.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import s from './CardsList.module.scss';
 
 const filterCards = (items, filter) => {
   return items.filter(cards =>
@@ -20,13 +21,13 @@ const Cards = ({option}) => {
   const newItemsTarget = filterCardsTarget(items, filter);
 
    return option === 'name' ? (
-    <ul>
+    <ul className={s.cards}>
        {newItems.map(({ name, id, target, bodyPart, equipment, gifUrl }) => (
-            <li key={id}>
-              <p> Name: {name}</p>
-              <p>Target: {target}</p>
-              <p>Body part: {bodyPart}</p>
-              <p>Equipment: {equipment}</p>
+            <li className={s.card} key={id}>
+              <p className={s.cardInfo}> Name: {name}</p>
+              <p className={s.cardInfo}>Target: {target}</p>
+              <p className={s.cardInfo}>Body part: {bodyPart}</p>
+              <p className={s.cardInfo}>Equipment: {equipment}</p>
               
             </li>
        ))}
--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -7,23 +7,23 @@ const Filter = ({handleChangeOption, option})  => {
   const filter = useSelector(state => state.cards.filter);
   
    return (
-     <>
-     <div>
-     <label htmlFor="" name="filter" > Sort by : </label>
-      <select className={s.selectSearch} name="" value={option}  onChange={handleChangeOption}>
-        <option value='default'>---</option>
-        <option value='name'>name</option>
-        <option value='target'>target</option>
-      </select>
-     </div>
-      <input 
-          className={s.inputSearch}
-          name="filter"
-          type="text" 
-          placeholder="enter your search" 
-          value={filter}
-          onChange={(event) => dispatch(changeFilter(event))}/>
-      </>
+     <div className={s.selectInputLabel}>
+        <div className={s.selectInput}>
+          <label className={s.selectLabel} htmlFor="" name="filter" > Sort by : </label>
+          <select className={s.selectSearch} name="" value={option}  onChange={handleChangeOption}>
+            <option value='default'>-----</option>
+            <option value='name'>name</option>
+            <option value='target'>target</option>
+          </select>
+        </div>
+          <input 
+              className={s.inputSearch}
+              name="filter"
+              type="text" 
+              placeholder="enter your search" 
+              value={filter}
+              onChange={(event) => dispatch(changeFilter(event))}/>
+      </div>
    );
  };
  
